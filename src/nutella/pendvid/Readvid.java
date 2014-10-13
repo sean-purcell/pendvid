511bb261d98dfd53c84f2353606d274613619b5a
Iburinoc <iburinoc@gmail.com>
2014-10-13 11:38:15 -0400

    removed pendvid, all it did was call readvid

--- src/nutella/pendvid/Readvid.java
+++ src/nutella/pendvid/Readvid.java
@@ -102,4 +102,13 @@ public class Readvid {
 			container.close();
 		}
 	}
+
+	public static void main(String[] args) {
+		if(args.length < 2) {
+			throw new IllegalArgumentException
+				("must pass filename as first argument and out dir as second");
+		}
+
+		Readvid.readvid(args[0], args[1]);
+	}
 }
