1740a91a7edfb66ad5137571fb21dbcb2d34be85
Andrey Boris Khesin <andrey.khesin@gmail.com>
2014-10-15 22:58:34 -0400

    "created method for finding pixels that are different" -Andrey

--- src/nutella/pendvid/ImgAnalyze.java
+++ src/nutella/pendvid/ImgAnalyze.java
@@ -1,5 +1,20 @@
 package nutella.pendvid;
 
-public class ImgAnalyze {
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
+public class ImgAnalyze {
+    public boolean[][] diff(BufferedImage a, BufferedImage b) {
+        boolean[][] d = new boolean[a.getWidth()][a.getHeight()];
+        for (int i = 0; i < a.getWidth(); i++) {
+            for (int j = 0; j < a.getHeight(); j++) {
+                Color o = new Color(a.getRGB(i, j));
+                Color n = new Color(b.getRGB(i, j));
+                d[i][j] = (o.getRed() - n.getRed()) * (o.getRed() - n.getRed()) +
+                        (o.getBlue() - n.getBlue()) * (o.getBlue() - n.getBlue()) +
+                      (o.getGreen() - n.getGreen()) * (o.getGreen() - n.getGreen()) > 400;
+            }
+        }
+        return d;
+    }
 }
